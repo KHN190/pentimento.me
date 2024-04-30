88bda447e09d8fa90b1f4ee7417583988558a427
Lingxiao Yu <knh190@hotmail.com>
2024-04-29 22:16:05 -0700

    Update theme (#2)
    * use accent color. disable media upload.
    
    * inc poll size. inc toot length. change css color.
    
    * fix str to int.
    
    * update li style
    
    * update toot icons.
    
    * update toot icons.

--- app/javascript/mastodon/features/status/components/action_bar.jsx
+++ app/javascript/mastodon/features/status/components/action_bar.jsx
@@ -14,13 +14,13 @@ import BookmarkBorderIcon from '@/material-icons/400-24px/bookmark.svg?react';
 import MoreHorizIcon from '@/material-icons/400-24px/more_horiz.svg?react';
 import RepeatIcon from '@/material-icons/400-24px/repeat.svg?react';
 import ReplyIcon from '@/material-icons/400-24px/reply.svg?react';
-import ReplyAllIcon from '@/material-icons/400-24px/reply_all.svg?react';
+import ReplyAllIcon from '@/material-icons/400-24px/reply.svg?react';
 import StarIcon from '@/material-icons/400-24px/star-fill.svg?react';
 import StarBorderIcon from '@/material-icons/400-24px/star.svg?react';
-import RepeatActiveIcon from '@/svg-icons/repeat_active.svg?react';
-import RepeatDisabledIcon from '@/svg-icons/repeat_disabled.svg?react';
-import RepeatPrivateIcon from '@/svg-icons/repeat_private.svg?react';
-import RepeatPrivateActiveIcon from '@/svg-icons/repeat_private_active.svg?react';
+import RepeatActiveIcon from '@/material-icons/400-24px/repeat.svg?react'; // infinit loop
+import RepeatDisabledIcon from '@/material-icons/400-24px/visibility_off-fill.svg?react';
+import RepeatPrivateIcon from '@/material-icons/400-24px/visibility-fill.svg?react';
+import RepeatPrivateActiveIcon from '@/material-icons/400-24px/visibility.svg?react';
 import { PERMISSION_MANAGE_USERS, PERMISSION_MANAGE_FEDERATION } from 'mastodon/permissions';
 import { WithRouterPropTypes } from 'mastodon/utils/react_router';
 
