c1adc7ce4e90bda8d3180d78551ffaabea05f322
knh190 <knh190@hotmail.com>
2024-07-04 13:01:29 -0700

    fix js ci.

--- app/javascript/mastodon/features/compose/components/compose_form.jsx
+++ app/javascript/mastodon/features/compose/components/compose_form.jsx
@@ -19,7 +19,7 @@ import EmojiPickerDropdown from '../containers/emoji_picker_dropdown_container';
 import PollButtonContainer from '../containers/poll_button_container';
 import PrivacyDropdownContainer from '../containers/privacy_dropdown_container';
 import SpoilerButtonContainer from '../containers/spoiler_button_container';
-import UploadButtonContainer from '../containers/upload_button_container';
+// import UploadButtonContainer from '../containers/upload_button_container';
 import WarningContainer from '../containers/warning_container';
 import { countableText } from '../util/counter';
 
