ac1ebb367dc0525ca580fd91a48a020510db0a24
knh190 <knh190@hotmail.com>
2024-07-04 13:03:37 -0700

    fix yarn format ci.

--- app/javascript/mastodon/features/status/components/action_bar.jsx
+++ app/javascript/mastodon/features/status/components/action_bar.jsx
@@ -12,14 +12,14 @@ import BookmarkIcon from '@/material-icons/400-24px/bookmark-fill.svg?react';
 import BookmarkBorderIcon from '@/material-icons/400-24px/bookmark.svg?react';
 import MoreHorizIcon from '@/material-icons/400-24px/more_horiz.svg?react';
 import RepeatIcon from '@/material-icons/400-24px/repeat.svg?react';
+import RepeatActiveIcon from '@/material-icons/400-24px/repeat.svg?react'; // infinit loop
 import ReplyIcon from '@/material-icons/400-24px/reply.svg?react';
 import ReplyAllIcon from '@/material-icons/400-24px/reply.svg?react';
 import StarIcon from '@/material-icons/400-24px/star-fill.svg?react';
 import StarBorderIcon from '@/material-icons/400-24px/star.svg?react';
-import RepeatActiveIcon from '@/material-icons/400-24px/repeat.svg?react'; // infinit loop
-import RepeatDisabledIcon from '@/material-icons/400-24px/visibility_off-fill.svg?react';
 import RepeatPrivateIcon from '@/material-icons/400-24px/visibility-fill.svg?react';
 import RepeatPrivateActiveIcon from '@/material-icons/400-24px/visibility.svg?react';
+import RepeatDisabledIcon from '@/material-icons/400-24px/visibility_off-fill.svg?react';
 import { identityContextPropShape, withIdentity } from 'mastodon/identity_context';
 import { PERMISSION_MANAGE_USERS, PERMISSION_MANAGE_FEDERATION } from 'mastodon/permissions';
 
